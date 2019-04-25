128bed371b88b38ccc9c4ba7f87dd0d92ab4d459
Alejandro Cruz <vidriloco@gmail.com>
2019-04-25 12:38:53 -0500

    Refactors AppStack router

--- src/pages/AppStack.js
+++ src/pages/AppStack.js
@@ -9,53 +9,15 @@ import OfferPage from './OfferPage';
 class AppStack extends Component {
 	render() {
 		// logged in
-		if(false) {
+		var token = localStorage.getItem('token') || '';
+		if(token.length == 0) {
 			return <IonPage>
-			    <Route exact path="/" render={() => <Redirect to="/landing"/>}/>
-			    {
-			    /**
-			     * Only render exact matches.  Only destroy on back button click
-			     * On history.push keep previous route stored for back button
-			     *
-			     * TabBar does a push on iontabbutton click.
-			     * TabBar updates the tab links based on the current route path.
-			     */
-			    }
-
-			    <IonTabs>
-						{ this.renderRoutesAndComponents() }
-			      <IonTabBar slot="bottom">
-			        <IonTabButton tab="fishes" href="/">
-			          <IonIcon name="sunny" />
-			          <IonLabel>Our Fish</IonLabel>
-			        </IonTabButton>
-			        <IonTabButton tab="branches" href="/sign-up">
-			          <IonIcon name="planet" />
-			          <IonLabel>Branches</IonLabel>
-			        </IonTabButton>
-			        <IonTabButton tab="map" href="/map">
-			          <IonIcon name="map" />
-			          <IonLabel>Map</IonLabel>
-			        </IonTabButton>
-			        <IonTabButton tab="about" href="/about">
-			          <IonIcon name="information-circle" />
-			          <IonLabel>About</IonLabel>
-			        </IonTabButton>
-			      </IonTabBar>
-			    </IonTabs>
+					<Route exact path="/" render={() => <Redirect to="/landing"/>}/>
+					{ this.renderRoutesAndComponents() }
 			  </IonPage>
 		} else {
 			return <IonPage>
-			    <Route exact path="/"/>
-			    {
-			    /**
-			     * Only render exact matches.  Only destroy on back button click
-			     * On history.push keep previous route stored for back button
-			     *
-			     * TabBar does a push on iontabbutton click.
-			     * TabBar updates the tab links based on the current route path.
-			     */
-			    }
+			    <Route exact path="/" render={() => <Redirect to="/feed"/>}/>
 			    { this.renderRoutesAndComponents() }
 			  </IonPage>
 		}
@@ -66,7 +28,8 @@ class AppStack extends Component {
 			<Route path="/:tab(landing)" component={LandingPage} exact={true} />
 			<Route path="/:tab(sign-up)" component={SignUpPage} exact={true} />
 			<Route path="/:tab(feed)" component={FeedPage} exact={true} />
-			<Route path="/:tab(offer)" component={OfferPage} exact={true} />
+			<Route path="/:tab(new-offer)" component={OfferPage} exact={true} />
+			<Route path="/:tab(edit-offer)" component={OfferPage} exact={true} />
 		</IonRouterOutlet>;
 	}
 }
