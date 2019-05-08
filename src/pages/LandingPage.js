9bf9feeab7f50bdb560b98c3fe286a730e8bc57c
Alejandro Cruz <vidriloco@gmail.com>
2019-05-08 16:22:23 -0500

    Adds loading indicator for user session creation

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,56 +3,96 @@ import { IonContent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonC
 import logo from '../recyclo-logo.svg';
 import Backend from '../Backend';
 
-class LandingPage extends Component {
-	constructor(props) {
-		super(props);
+import { ClipLoader } from 'react-spinners';
 
-		this.state = {
+class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
 			login: null,
 			password: null
 		}
 		
 		this.updateField = this.updateField.bind(this);
 		this.loginAccount = this.loginAccount.bind(this);
-  }
-	
-  render() {
-    return (
-		<IonContent>
-			<IonCard>
-				<img src={logo} className="App-logo" alt="logo" />
-			</IonCard>
-			<IonCard>
-				<img alt="" src="https://media.giphy.com/media/l1KVcrdl7rJpFnY2s/giphy.gif" />
-		      	<IonCardHeader>
-		        	<IonCardTitle>Bienvenido</IonCardTitle>
-					<IonCardSubtitle>Si ya tienes una cuenta, inicia sesión</IonCardSubtitle>
-		      	</IonCardHeader>
+    }
+    
+    render() {
+        return (
+		    <IonContent>
+			    <IonCard>
+				    <img src={logo} className="App-logo" alt="logo" />
+			    </IonCard>
+                { this.renderActionCards() }
+            </IonContent>
+        );
+    }
+  
+    renderActionCards() {
+        
+        var newAccountInvitationCard = null;
+        
+        if(!this.state.isGettingLoggedIn) {
+            newAccountInvitationCard = this.renderNewAccountInvitationCard();
+        }
+        
+        return <div>
+            { this.renderLoginCard() }
+            { newAccountInvitationCard }
+        </div>
+    }
+    
+    renderNewAccountInvitationCard() {
+        return <IonCard>
+              <IonCardHeader>
+                  <IonCardTitle>Nuevo?</IonCardTitle>
+                  <IonCardSubtitle>Empieza a publicar tus reciclables hoy</IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                  <IonButton expand="block" href="/sign-up">Crear cuenta</IonButton>
+              </IonCardContent>
+          </IonCard>
+    }
+    
+    renderLoginCard() {
+        var defaultContent = <div>
+            <IonCardHeader>
+                <IonCardTitle>Bienvenido</IonCardTitle>
+                <IonCardSubtitle>Si ya tienes una cuenta, inicia sesión</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+                <IonInput id="login" placeholder="Nombre de usuario o Email" type="email" value={this.state.login} onIonChange={this.updateField}></IonInput>
+                <IonInput id="password" placeholder="Contraseña" type="password" value={this.state.password} onIonChange={this.updateField}></IonInput>
+  
+                <IonButton expand="block" color="warning" onClick={this.loginAccount}>Entrar</IonButton>
+            </IonCardContent>
+        </div>
+        
+        if(this.state.isGettingLoggedIn) {
+            defaultContent = <div className="ion-text-center">
+                <br/><br/>
+                <ClipLoader
+                    sizeUnit={"px"}
+                    size={40}
+                    color={'#FC7213'}
+                    loading={true} />
+        	    <p className="ion-text-center page-subtitle">Iniciando sesión ...</p>
+            </div> 
+        }
+        
+        return <IonCard>
+          <img alt="" src="https://media.giphy.com/media/l1KVcrdl7rJpFnY2s/giphy.gif" />
 
-		      	<IonCardContent>
-					<IonInput id="login" placeholder="Nombre de usuario o Email" type="email" value={this.state.login} onIonChange={this.updateField}></IonInput>
-				
-					<IonInput id="password" placeholder="Contraseña" type="password" value={this.state.password} onIonChange={this.updateField}></IonInput>
-					<IonButton expand="block" color="warning" onClick={this.loginAccount}>Entrar</IonButton>
-		      	</IonCardContent>
-		    	</IonCard>
-				
-					<IonCard>
-		      	<IonCardHeader>
-		        	<IonCardTitle>Nuevo?</IonCardTitle>
-					<IonCardSubtitle>Empieza a publicar tus reciclables hoy</IonCardSubtitle>
-		      	</IonCardHeader>
-				<IonCardContent>
-					<IonButton expand="block" href="/sign-up">Crear cuenta</IonButton>
-				</IonCardContent>
-	    	</IonCard>
-		</IonContent>
-    );
-  }
-	
+          { defaultContent }
+        </IonCard>
+    }
+  	
 	loginAccount() {
 		var data = { session: this.state };
-
+        
+        this.setState({ isGettingLoggedIn: true });
+        
 		var result = fetch(Backend.sessions('create'), {
         	method: 'POST',
         	body: JSON.stringify(data),
@@ -62,10 +102,12 @@ class LandingPage extends Component {
         		if (!response.ok) { throw response }
         		return response.json();
 			}).then(json => {
+                this.setState({ isGettingLoggedIn: false });
 				var tokenValue = json.token;
 				localStorage.setItem('token', tokenValue);
 				this.props.history.push("/");
 			}).catch(error => {
+                this.setState({ isGettingLoggedIn: false });
 				error.json().then(jsonError => {
 					alert(jsonError.error);
 				})
